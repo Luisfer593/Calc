7a48adc0dcc97e9776a17c38f5f2b5aadd50dfe6
Luisfer593 <luisfercuji_@hotmail.com>
2021-12-01 18:16:41 -0500

    add clase

---
+++ Calculadora/ClsPractica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class ClsPractica
+    {
+        private string user;
+        private int num;
+
+    }
+}
